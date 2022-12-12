bf56d2ae729083882b6cab8f2d2436bf6441e79f
Jafar_AD@hotmail.com <Jafar_AD@hotmail.com>
2022-12-12 14:31:06 +0000

    arrays

---
+++ arrays.js
@@ -0,0 +1,76 @@
+// let favHolidayDestinations = [
+//     "Venice, Italy",
+//     "Paris, France",
+//     "Barcelona, Spain"
+// ]
+
+// console.log(favHolidayDestinations)
+
+
+// let topSixMovies = [
+//     "Troy", "Goodfellas",
+//     "Godfather", "Parasyte",
+//     "Ten things i hate about you", "city of God"
+// ]
+
+// console.log(topSixMovies[0])
+
+
+// let topSixMovies = [
+//     "Troy", "Goodfellas",
+//     "Godfather", "Parasyte",
+//     "Ten things i hate about you", "city of God"
+// ]
+
+// console.log(topSixMovies)
+
+// topSixMovies[1] = "Bama"
+// console.log(topSixMovies)
+
+
+
+// let topSixMovies = [
+//     "Troy", "Goodfellas",
+//     "Godfather", "Parasyte",
+//     "Ten things i hate about you", "city of God"
+// ]
+
+// console.log(topSixMovies)
+
+// topSixMovies[1] = "Bama"
+// console.log(topSixMovies.length)
+
+// let topSixMovies = [
+//     "Troy", "Goodfellas",
+//     "Godfather", "Parasyte",
+//     "Ten things i hate about you", "city of God"
+// ]
+
+
+// console.log(topSixMovies.shift())
+
+
+// let favHolidayDestinations = [
+//     "Venice", "Italy",
+//     "Paris", "France",
+//     "Barcelona", "Spain"
+// ]
+
+// for (let J = 0; J <favHolidayDestinations.length; J++){
+//     console.log(favHolidayDestinations[J])
+// }
+
+
+
+for (let i = 0; i<= 10; i++){
+    console.log(i += 2)
+}
+
+let evenNumbers = []
+
+for (let n = 0; n < 20; n++){
+    if (n % 2 == 0) {
+        (evenNumbers.push(n))
+    }
+}
+console.log(`The even numbers between 0 and 20 are; ${evenNumbers}`)
