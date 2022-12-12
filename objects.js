15845ab11101e4c1823adf298406b758d240bca8
Jafar_AD@hotmail.com <Jafar_AD@hotmail.com>
2022-12-12 17:22:40 +0000

    more code

--- objects.js
+++ objects.js
@@ -97,26 +97,59 @@
 // }
 
 
-const person = {
-    firstName: "Sally",
-    lastName:"Evans",
-    age: 27,
-    occupation: "Sales assistant",
-    married: false,
-    homeOwner: false,
-    hobbies: ["Tennis","Gardening", "Bungee Jumping"],
-    marketingOp () {
-        if (this.HomeOwner == false && this.age > 25){
-            return "send mortgage offer email."
-        }
-        else if (this.HomeOwner == false && this.age < 25){
-            return "pension infestment offer."
-        }
-        else {
-            return "send sensible Savings offer."
-        }
-    }
+// const person = {
+//     firstName: "Sally",
+//     lastName:"Evans",
+//     age: 27,
+//     occupation: "Sales assistant",
+//     married: false,
+//     homeOwner: false,
+//     hobbies: ["Tennis","Gardening", "Bungee Jumping"],
+//     marketingOp () {
+//         if (this.HomeOwner == false && this.age > 25){
+//             return "send mortgage offer email."
+//         }
+//         else if (this.HomeOwner == false && this.age < 25){
+//             return "pension infestment offer."
+//         }
+//         else {
+//             return "send sensible Savings offer."
+//         }
+//     }
 
+// }
+
+// console.log(person.marketingOp())
+
+
+class Pet {
+    constructor(name, age, type, breed, colour, time) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.breed = breed;
+        this.colour = colour;
+        this.lastFed = time;
+    }
+    feedPet (time) {
+        this.lastFed = time;
+        return `Last feed updated to: ${this.lastFed} for ${this.name}`;
+    }
 }
 
-console.log(person.marketingOp())
+const bella = new Pet("Bella", 2, "Dog", "GS", "B&T", "8:00");
+console.log(bella.feedPet("15:00"));
+
+class PetMeds extends Pet {
+    constructor(name, age, type, breed, colour, time, mtime) {
+        super(name, age, type, breed, colour, time);
+    }
+    givesMeds (time) {
+        this.lastMeds = time;
+        return `Last meds updated to: ${this.lastMeds}  for ${this.name}`;
+    }
+}
+
+const Bella = new Pet("Bella", 2, "Dog", "GS", "B&T", "8:00");
+const Barney = new PetMeds("Barney", 4, "Cat", "Don-short", "B&G", "10:00", "13:00" );
+const Betty = new Pet("Betty", 4, "Dog", "SH", "White", "10:00");
